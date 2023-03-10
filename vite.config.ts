227751a5e35200624fb54016b5dba108cb045f8d
Kyza <kyza@kyza.net>
2023-03-10 18:40:42 -0500

    build: import solid-start-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import pomsky from "@pomsky-lang/unplugin";
+import solidStartVercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import { UserConfigExport } from "vite";
 
 export default {
 	plugins: [
 		solid({
-			adapter: vercel({ edge: false }),
+			adapter: solidStartVercel({ edge: false }),
 		}),
 		pomsky.vite({
 			flavor: "js",
